2d6212db0b845086cfe77d20744230b9d75b8097
Puneet Yadav <puneety259@gmail.com>
2025-09-14 01:05:18 +0530

    Add payment model and integrate Razorpay checkout script in index.html

---
+++ backend/src/models/payment.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+    product_id:{
+        type: mongoose.Types.Schema.ObjectId,
+        ref: "product",
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Success","Pending","Failed"],
+        default: "Pending",
+        required: true
+    },
+    user: {
+        type: mongoose.Types.Schema.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    payment_id: {
+        type: String,
+    }
+})
+
+const paymentModel = mongoose.model("payment", paymentSchema);
+
+module.exports = paymentModel;
