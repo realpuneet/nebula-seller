b2e9678f80301295ebfc5fed7783fff51a6d250d
Puneet Yadav <puneety259@gmail.com>
2025-09-17 15:36:38 +0530

    Update CORS configuration to allow requests from the production URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://nebula-seller.vercel.app",
     credentials: true,
   })
 );
